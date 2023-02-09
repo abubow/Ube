b0edc8e64bdcb00bf0e1250336b7cd55c979abc1
Syed Mohammad Abuzar <40357673+abubow@users.noreply.github.com>
2023-02-09 16:14:36 +0500

    added vercel files

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+# upgrade pip
+pip install --upgrade pip
 
 # Install dependencies
 pip install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic --noinput
+# python manage.py collectstatic --noinput
+
+# Apply database migrations
+python manage.py migrate
